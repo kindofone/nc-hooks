f60a1394ff62dd5b6faf3a51088f68a09cb5c846
Yogev Ahuvia <yogev.ahuvia@gmail.com>
2022-02-02 08:49:26 +0200

    http server that returns a hard coded posts json

--- server/server.mjs
+++ server/server.mjs
@@ -1,1 +1,25 @@
-// TODO: Build server
+import http from 'http';
+import Url from 'url-parse';
+import {POSTS_JSON} from './posts_json.mjs';
+
+const hostname = '127.0.0.1';
+const port = '3001';
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Max-Age', 2592000);
+  res.setHeader('Content-Type', 'application/json');
+
+  const {query} = new Url(req.url, true);
+  const id = parseInt(query.postsId);
+  const posts = JSON.parse(POSTS_JSON);
+  const post = posts.find(post => post.id === id);
+
+  res.end(JSON.stringify(post));
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
